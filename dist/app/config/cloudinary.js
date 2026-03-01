0c3acc10e94efea52f03e27424a3d6f742cca28d
FLAVIO GOMES SOARES <FlavioGomesSoares2007@gmail.com>
2026-03-01 14:11:18 -0300

    a2

--- dist/app/config/cloudinary.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const cloudinary_1 = require("cloudinary");
-cloudinary_1.v2.config({
-    secure: true,
-});
-exports.default = cloudinary_1.v2;
